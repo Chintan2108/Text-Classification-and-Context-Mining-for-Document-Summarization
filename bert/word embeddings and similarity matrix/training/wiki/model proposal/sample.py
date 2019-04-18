a0d28ae7da87a66f8c4351eb464afba1ba4f831a
Chintan2108 <chintanmaniyar@gmail.com>
2019-04-18 19:50:13 +0530

    model proposal - sampled more wiki data

--- bert/word embeddings and similarity matrix/training/wiki/model proposal/sample.py
+++ bert/word embeddings and similarity matrix/training/wiki/model proposal/sample.py
@@ -1,21 +1,27 @@
 import wikipedia, pickle, os
 import pandas as pd
 
+WHITELIST = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 01234567890')
+
 def talkComments():
     '''
     Store talk comments
     '''
     comments = []
     articleName = []
-    TALK_COMMENTS_PATH = './raw/tc'
+    TALK_COMMENTS_PATH = './raw/tcnew'
     articles = os.listdir(TALK_COMMENTS_PATH)
     df = pd.DataFrame(columns=['Comments', 'Article'])
+    globalRowCount = 0
     for article in articles:
         if article.endswith('csv'):
             tc = pd.read_csv(os.path.join(TALK_COMMENTS_PATH, article))
             comments.extend(tc['text'])
             for i in range(len(tc['text'])):
                 articleName.append(article.split('.')[0].replace('_',' '))
+                comments[i + globalRowCount] = ''.join(filter(WHITELIST.__contains__, str(comments[i + globalRowCount])))
+                comments[i + globalRowCount] = article.split('.')[0].replace('_',' ') + '\t' + comments[i + globalRowCount] 
+            globalRowCount += (i+1)
             print('Parsed %s successfully.' % article)
             print('*******************')
     df['Comments'] = comments
@@ -32,7 +38,8 @@ def articles():
     '''
     articleSentences = []
     articleName = []
-    titles = open('./../dataset/train/wikiTrainArticles.txt', 'r', encoding='utf-8').readlines()
+    titles = open('articleNames.txt', 'r', encoding='utf-8').readlines()
+    # titles = open('./../dataset/train/wikiTrainArticles.txt', 'r', encoding='utf-8').readlines()
     df = pd.DataFrame(columns=['Sentence', 'Article'])
     for title in titles:
         articleContent = wikipedia.page(title.split('\n')[0])
@@ -55,4 +62,4 @@ def articles():
         pickle.dump(articleSentences, temp)
 
 talkComments()
-    articles()
+articles()
